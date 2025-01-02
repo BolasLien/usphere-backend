ccf23e9217fe2c826621e5e863efc2f85fc65116
Bolas Lien <ltzuen@gmail.com>
2025-01-03 01:09:29 +0800

    feat: add CORS middleware for handling cross-origin requests (#6)
    * feat: add CORS middleware for handling cross-origin requests
    
    * feat: 設定 CORS 中介軟體以允許多個來源
    
    * feat: 使用環境變數設定 CORS 允許的來源

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const express = require('express');
-const userRoutes = require('./src/routes/userRoutes');
-const topicRoutes = require('./src/routes/topicRoutes');
-const logger = require('./src/utils/logger');
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./src/routes/userRoutes");
+const topicRoutes = require("./src/routes/topicRoutes");
+const logger = require("./src/utils/logger");
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-app.use('/users', userRoutes);
-app.use('/topics', topicRoutes);
+require("dotenv").config();
 
-logger.info('Express app 已經啟動');
+// 設定 CORS 中介軟體，允許多個來源
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : [];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.use(cors(corsOptions)); // 使用 cors 中介軟體
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/topics", topicRoutes);
+
+logger.info("Express app 已經啟動");
 // ...existing code...
 
 app.listen(port, () => {
