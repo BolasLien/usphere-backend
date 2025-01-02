c1b301120766351744b639ea37249ceb47b81bf2
Bolas <ltzuen@gmail.com>
2025-01-02 22:45:53 +0800

    feat: 設定 CORS 中介軟體以允許多個來源

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const logger = require('./src/utils/logger');
 const app = express();
 const port = 3000;
 
-app.use(cors()); // 使用 cors 中介軟體
+// 設定 CORS 中介軟體，允許多個來源
+const allowedOrigins = ['http://localhost:5173', 'https://wangyingju.github.io/USphere/'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions)); // 使用 cors 中介軟體
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/topics', topicRoutes);
