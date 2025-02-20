d94af44366dc3624cfa1fa04f84202e1302f6239
Bolas <ltzuen@gmail.com>
2025-02-20 23:03:44 +0800

    feat(comment): 新增身份驗證中介軟體以支援留言功能

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const router = express.Router();
 const commentController = require("../controllers/commentController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const router = express.Router();
 
 router.get("/topics/:id/comments", commentController.getCommentsByTopicId);
-router.post("/topics/:id/comments", commentController.createComment);
-router.post("/topics/:id/comments/:commentId/like", commentController.likeComment);
+router.post(
+  "/topics/:id/comments",
+  authMiddleware,
+  commentController.createComment
+);
 
 module.exports = router;
