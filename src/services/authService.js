48038e2670adb928704be34b04cf30c5296bc17d
Bolas <ltzuen@gmail.com>
2025-02-21 00:30:04 +0800

    feat(auth): 更新登入功能以包含用戶詳細資訊

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,21 +1,28 @@
 const supabase = require("../utils/db");
+const userService = require("./userService");
 
 const loginUser = async (email, password) => {
   if (!email || !password) {
-    throw { status: 400, message: 'Email 與密碼必填' };
+    throw { status: 400, message: "Email 與密碼必填" };
   }
 
-  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
   if (error) {
-    throw { status: 401, message: '帳號或密碼錯誤' };
+    throw { status: 401, message: "帳號或密碼錯誤" };
   }
+
+  const user = await userService.getUserById(data.user.id);
 
   return {
     access_token: data.session.access_token,
     user: {
-      id: data.user.id,
-      email: data.user.email,
+      id: user.id,
+      name: user.display_name,
+      pic: user.profile_pic_url,
     },
   };
 };
