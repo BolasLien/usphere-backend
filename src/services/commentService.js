b1c525a4366c0e13fd1ccb488f6cb694f812cad8
Bolas Lien <ltzuen@gmail.com>
2025-03-02 23:37:04 +0800

    feat(comment): 更新留言查詢以使用視圖並簡化回傳資料 (#25)

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -3,22 +3,14 @@ const db = require("../utils/db");
 // 獲取指定話題下的所有留言
 exports.getCommentsByTopicId = async (topicId) => {
   const { data, error } = await db
-    .from("comments")
-    .select("*, users(display_name,profile_pic_url),likes(count)")
+    .from("comments_view")
+    .select("*")
     .eq("topic_id", topicId)
     .order("created_at", { ascending: false });
 
   if (error) throw new Error(error.message);
 
-  const comments = data.map((comment) => {
-    comment.user_name = comment.users ? comment.users.display_name : "未知用戶";
-    comment.user_pic = comment.users ? comment.users.profile_pic_url : "";
-    comment.likes = comment.likes[0].count;
-    const { user_id, users, topic_id, created_at, ...data } = comment;
-    return data;
-  });
-
-  return comments;
+  return data;
 };
 
 // 在指定話題下發表新留言
